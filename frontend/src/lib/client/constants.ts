4d345ae35756aca8fe4b0b9c383574664709183c
Joshua McClung <josh.mcclung66@gmail.com>
2024-04-04 15:53:30 -0600

    switch to env approach for secrets

---
+++ frontend/src/lib/client/constants.ts
@@ -0,0 +1,5 @@
+// export const strapi_url = "http://localhost:1337";
+import { PUBLIC_STRAPI_URL, PUBLIC_GITHUB_URL } from '$env/static/public';
+export const strapi_url = PUBLIC_STRAPI_URL
+export const github_url = PUBLIC_GITHUB_URL
+// export const github_url = "https://github.com/jeffeth-donaldson";
---
+++ frontend/src/lib/server/secrets.ts
@@ -0,0 +1,2 @@
+import { STRAPI_KEY } from "$env/static/private"
+export const strapi_key = STRAPI_KEY
